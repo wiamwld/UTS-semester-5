9fe0e49c8612c70f2fde8a88164d5ac24193d86e
wiam <wiamwulandari10@gmail.com>
2023-11-06 12:40:43 +0700

    app

---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+import logo from "./logo.png";
+import Splashbackground from "./Splashbackground.png";
+
+export {logo, Splashbackground}
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import Splash from './Splash';
+import Akun from './Akun';
+import Pesanan from './Pesanan';
+
+export { Home, Splash, Akun, Pesanan }
